fcdd86ad1e4cec5e8d3c1c461aea4fde498dc6b3
Rashidul Islam <rashidul0405@gmail.com>
2017-11-07 09:16:55 +0600

    extract clock component

--- src/2.ClockApp.js
+++ src/2.ClockApp.js
@@ -3,22 +3,25 @@ import * as moment from 'moment-timezone'
 
 export default class ClockApp extends Component{
     render(){
-        const newYork = moment().tz("America/New_York").format("dddd, MMMM Do YYYY, h:mm:ss a zz")
-        const oslo = moment().tz("Europe/Oslo").format("dddd, MMMM Do YYYY, h:mm:ss a zz")
-        const dhaka = moment().tz("Asia/Dhaka").format("dddd, MMMM Do YYYY, h:mm:ss a zz")
-
         return (
             <div>
                 <h1>Your Ultimate Time App!</h1>
-                <div>
-                    <b>America/New_York: </b> <span>{newYork}.</span>
-                </div>
-                <div>
-                    <b>Europe/Oslo: </b> <span>{oslo}.</span>
-                </div>
-                <div>
-                    <b>Asia/Dhaka: </b> <span>{dhaka}.</span>
-                </div>
+                <Clock zone="America/New_York" />
+                <Clock zone="Europe/Oslo" />
+                <Clock zone="Asia/Dhaka" />
+            </div>
+        )
+    }
+}
+
+const Clock = class extends Component{
+    render(){
+        const timeZone = this.props.zone
+        const time = moment().tz(timeZone).format("dddd, MMMM Do YYYY, h:mm:ss a zz")
+
+        return (
+            <div>
+                <b>{timeZone}: </b> <span>{time}.</span>
             </div>
         )
     }
