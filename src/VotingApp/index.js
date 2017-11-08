fc04dc2a9c8e31b3a1b00fe345e70a31c514fe12
Rashidul Islam <rashidul0405@gmail.com>
2017-11-08 09:28:52 +0600

    initial template for app

--- src/VotingApp/index.js
+++ src/VotingApp/index.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react'
 
 import './style.css'
-import ProductList from './ProductList'
-
 
 export default class VotingApp extends Component{
     render(){
@@ -10,7 +8,93 @@ export default class VotingApp extends Component{
             <div className="main ui text container">
                 <h1 className="ui dividing centered header">Popular Products</h1>
                 <div id="content">
-                    <ProductList/>
+                    <div className='ui unstackable items'>
+                        <div className='item'>
+                            <div className='image'>
+                                <img src="images/products/image-yellow.png" />
+                            </div>
+                            <div className='middle aligned content'>
+                                <div className='header'>
+                                    <a>
+                                        <i className='large caret up icon' />
+                                    </a>
+                                    50
+                                </div>
+                                <div className='description'>
+                                    <a href="#">
+                                        Yellow Pail
+                                    </a>
+                                    <p>
+                                        On-demand sand castle construction expertise.
+                                    </p>
+                                </div>
+                                <div className='extra'>
+                                    <span>Submitted by:</span>
+                                    <img
+                                        className='ui avatar image'
+                                        src="images/avatars/molly.png"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='item'>
+                            <div className='image'>
+                                <img src="images/products/image-yellow.png" />
+                            </div>
+                            <div className='middle aligned content'>
+                                <div className='header'>
+                                    <a>
+                                        <i className='large caret up icon' />
+                                    </a>
+                                    50
+                                </div>
+                                <div className='description'>
+                                    <a href="#">
+                                        Yellow Pail
+                                    </a>
+                                    <p>
+                                        On-demand sand castle construction expertise.
+                                    </p>
+                                </div>
+                                <div className='extra'>
+                                    <span>Submitted by:</span>
+                                    <img
+                                        className='ui avatar image'
+                                        src="images/avatars/molly.png"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='item'>
+                            <div className='image'>
+                                <img src="images/products/image-yellow.png" />
+                            </div>
+                            <div className='middle aligned content'>
+                                <div className='header'>
+                                    <a>
+                                        <i className='large caret up icon' />
+                                    </a>
+                                    50
+                                </div>
+                                <div className='description'>
+                                    <a href="#">
+                                        Yellow Pail
+                                    </a>
+                                    <p>
+                                        On-demand sand castle construction expertise.
+                                    </p>
+                                </div>
+                                <div className='extra'>
+                                    <span>Submitted by:</span>
+                                    <img
+                                        className='ui avatar image'
+                                        src="images/avatars/molly.png"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         )
