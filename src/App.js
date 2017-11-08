9cfef24f781ce700c249ea9439e603036b4105b7
Rashidul Islam <rashidul0405@gmail.com>
2017-11-08 09:19:17 +0600

    complete voting app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import HelloWorld from './0.HelloWorld';
 import Greetings from './1.Greetings';
 import ClockApp from './2.ClockApp'
+import VotingApp from './VotingApp'
 
 class App extends Component {
     render() {
         return (
             //<HelloWorld />
             //<Greetings name={'kamarul'}/>
-            <ClockApp />
+            //<ClockApp />
+            <VotingApp />
         );
     }
 }
